2cb25a3e868d4d0a0be454aa81c8b67e0b47bb82
codeFrite <codefrite@gmail.com>
2025-03-09 21:46:16 +0100

    moving Component & Tools (SelectionTools, ConnectionTools, SourceTools, SinkTool, ComponentTools) types & enums from canvas to types.ts

--- front-end/components/canvas/index.tsx
+++ front-end/components/canvas/index.tsx
@@ -5,44 +5,6 @@ import { v4 } from "uuid";
 import styles from "./index.module.css";
 import Grid from "../grid";
 
-type Component = {
-  id: string;
-  position: Position;
-};
-
-enum SelectionTools {
-  SELECT = "select",
-  DRAG = "drag",
-}
-
-enum ConnectionTools {
-  WIRE = "wire",
-  BUS = "bus",
-}
-
-enum SourceTools {
-  CLOCK = "clock", // switch between low & high logic level at a given frequency
-  BUTTON = "button", // set logic level to high when clicked
-  PULSE = "pulse", // set logic level to high for a given duration
-}
-
-enum SinkTools {
-  LED = "led",
-  SEVEN_SEGMENT_DISPLAY = "seven-segment-display",
-  OSCILLOSCOPE = "oscilloscope",
-}
-
-enum ComponentTools {
-  NOT_GATE = "not-gate",
-  AND_GATE = "and-gate",
-  NAND_GATE = "nand-gate",
-  OR_GATE = "or-gate",
-  NOR_GATE = "nor-gate",
-  USER_DEFINED = "user-defined",
-}
-
-type Tools = SelectionTools | ConnectionTools | SourceTools | SinkTools | ComponentTools;
-
 const Canvas = () => {
   const [dimensions, setDimensions] = useState<Dimension>({ width: 0, height: 0 });
   const [components, setComponents] = useState<Component[]>([]);
--- front-end/components/types.ts
+++ front-end/components/types.ts
@@ -7,3 +7,41 @@ type Dimension = {
   width: number;
   height: number;
 };
+
+type Component = {
+  id: string;
+  position: Position;
+};
+
+enum SelectionTools {
+  SELECT = "select",
+  DRAG = "drag",
+}
+
+enum ConnectionTools {
+  WIRE = "wire",
+  BUS = "bus",
+}
+
+enum SourceTools {
+  CLOCK = "clock", // switch between low & high logic level at a given frequency
+  BUTTON = "button", // set logic level to high when clicked
+  PULSE = "pulse", // set logic level to high for a given duration
+}
+
+enum SinkTools {
+  LED = "led",
+  SEVEN_SEGMENT_DISPLAY = "seven-segment-display",
+  OSCILLOSCOPE = "oscilloscope",
+}
+
+enum ComponentTools {
+  NOT_GATE = "not-gate",
+  AND_GATE = "and-gate",
+  NAND_GATE = "nand-gate",
+  OR_GATE = "or-gate",
+  NOR_GATE = "nor-gate",
+  USER_DEFINED = "user-defined",
+}
+
+type Tools = SelectionTools | ConnectionTools | SourceTools | SinkTools | ComponentTools;
