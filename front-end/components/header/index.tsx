2ff8b470fb8e51dfc3943b1c0872bdee87f8060a
codeFrite <codefrite@gmail.com>
2025-03-09 21:36:19 +0100

    adding the header component which contains the app logo & @codefrite mention

---
+++ front-end/components/header/index.tsx
@@ -0,0 +1,8 @@
+import styles from "./index.module.css";
+
+const Header: React.FC = () => (
+  <header className={styles.header}>
+    <h1>Digi-GOGO@codefrite.dev</h1>
+  </header>
+);
+export default Header;
