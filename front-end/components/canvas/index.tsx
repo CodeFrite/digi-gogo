027df0e56db32a57870b077887e0dc8323195ea7
codeFrite <codefrite@gmail.com>
2025-03-09 21:44:29 +0100

    grid: moving the grid component from canvas to its own folder since it will grow and contain more features (snap to grid, move grid, zoom in/out, ...)

--- front-end/components/canvas/index.tsx
+++ front-end/components/canvas/index.tsx
@@ -3,6 +3,7 @@
 import { useEffect, useState } from "react";
 import { v4 } from "uuid";
 import styles from "./index.module.css";
+import Grid from "../grid";
 
 type Component = {
   id: string;
@@ -41,52 +42,6 @@ enum ComponentTools {
 }
 
 type Tools = SelectionTools | ConnectionTools | SourceTools | SinkTools | ComponentTools;
-
-type GridProps = {
-  dimension: Dimension; // width and height of the canvas on which to render the grid
-  spacing?: number; // width and height of the grid squares
-  snapToGrid?: boolean; // snap components to the grid when moving them
-};
-
-const Grid: React.FC<GridProps> = (props: GridProps) => {
-  const dimension = props.dimension;
-  const gridSize = props.spacing ?? 25;
-
-  const drawGrid = () => {
-    const gridLines = [];
-    for (let i = 0; i < dimension.width; i += gridSize) {
-      gridLines.push(
-        <line
-          key={"grid-line-x-" + i}
-          x1={i}
-          y1={0}
-          x2={i}
-          y2={dimension.height}
-          stroke="gray"
-          strokeWidth={0.5}
-        />
-      );
-    }
-
-    for (let i = 0; i < dimension.height; i += gridSize) {
-      gridLines.push(
-        <line
-          key={"grid-line-y-" + i}
-          x1={0}
-          y1={i}
-          x2={dimension.width}
-          y2={i}
-          stroke="gray"
-          strokeWidth={0.5}
-        />
-      );
-    }
-
-    return gridLines;
-  };
-
-  return <g>{drawGrid()}</g>;
-};
 
 const Canvas = () => {
   const [dimensions, setDimensions] = useState<Dimension>({ width: 0, height: 0 });
---
+++ front-end/components/grid/index.tsx
@@ -0,0 +1,46 @@
+type GridProps = {
+  dimension: Dimension; // width and height of the canvas on which to render the grid
+  spacing?: number; // width and height of the grid squares
+  snapToGrid?: boolean; // snap components to the grid when moving them
+};
+
+const Grid: React.FC<GridProps> = (props: GridProps) => {
+  const dimension = props.dimension;
+  const gridSize = props.spacing ?? 25;
+
+  const drawGrid = () => {
+    const gridLines = [];
+    for (let i = 0; i < dimension.width; i += gridSize) {
+      gridLines.push(
+        <line
+          key={"grid-line-x-" + i}
+          x1={i}
+          y1={0}
+          x2={i}
+          y2={dimension.height}
+          stroke="gray"
+          strokeWidth={0.5}
+        />
+      );
+    }
+
+    for (let i = 0; i < dimension.height; i += gridSize) {
+      gridLines.push(
+        <line
+          key={"grid-line-y-" + i}
+          x1={0}
+          y1={i}
+          x2={dimension.width}
+          y2={i}
+          stroke="gray"
+          strokeWidth={0.5}
+        />
+      );
+    }
+
+    return gridLines;
+  };
+
+  return <g>{drawGrid()}</g>;
+};
+export default Grid;
