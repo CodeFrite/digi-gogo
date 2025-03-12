29aea729e0e543a1265483322eca23314f328195
codeFrite <codefrite@gmail.com>
2025-03-12 06:13:30 +0100

    Grid: change the way the grid is drawn to make sure it is redrawn whenever the grid spacing changes (command panel) or the window is resized

--- front-end/components/grid/index.tsx
+++ front-end/components/grid/index.tsx
@@ -1,4 +1,6 @@
-import { Dimension, GridSpacing } from "../types";
+import { JSX, useEffect, useState } from "react";
+import { GridSpacing } from "../config";
+import { Dimension } from "../types";
 
 type GridProps = {
   dimension: Dimension; // width and height of the canvas on which to render the grid
@@ -7,41 +9,45 @@ type GridProps = {
 };
 
 const Grid: React.FC<GridProps> = (props: GridProps) => {
-  const dimension = props.dimension;
+  const [gridLines, setGridLines] = useState<JSX.Element[]>([]);
 
-  const drawGrid = () => {
-    const gridLines = [];
-    for (let i = 0; i < dimension.width; i += props.spacing) {
-      gridLines.push(
-        <line
-          key={"grid-line-x-" + i}
-          x1={i}
-          y1={0}
-          x2={i}
-          y2={dimension.height}
-          stroke="gray"
-          strokeWidth={0.5}
-        />
-      );
-    }
+  useEffect(() => {
+    const drawGrid = () => {
+      const lines = [];
+      for (let i = 0; i < props.dimension.width; i += props.spacing) {
+        lines.push(
+          <line
+            key={"grid-line-x-" + i}
+            x1={i}
+            y1={0}
+            x2={i}
+            y2={props.dimension.height}
+            stroke="gray"
+            strokeWidth={0.5}
+          />
+        );
+      }
 
-    for (let i = 0; i < dimension.height; i += props.spacing) {
-      gridLines.push(
-        <line
-          key={"grid-line-y-" + i}
-          x1={0}
-          y1={i}
-          x2={dimension.width}
-          y2={i}
-          stroke="gray"
-          strokeWidth={0.5}
-        />
-      );
-    }
+      for (let i = 0; i < props.dimension.height; i += props.spacing) {
+        lines.push(
+          <line
+            key={"grid-line-y-" + i}
+            x1={0}
+            y1={i}
+            x2={props.dimension.width}
+            y2={i}
+            stroke="gray"
+            strokeWidth={0.5}
+          />
+        );
+      }
 
-    return gridLines;
-  };
+      return lines;
+    };
 
-  return <g>{drawGrid()}</g>;
+    setGridLines(drawGrid());
+  }, [props.dimension, props.spacing]);
+
+  return <g>{gridLines}</g>;
 };
 export default Grid;
